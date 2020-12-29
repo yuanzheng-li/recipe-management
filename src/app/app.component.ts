fc72a4a02ec69c2c7b2865ef1498ff05723d97c5
Yuanzheng Li <yuanzhengliwork@gmail.com>
2020-12-29 14:11:40 -0800

    navigation between recipes and shopping list components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-management';
+  selectedSection = 'recipes';
+
+  onNavigation(section: string) {
+    this.selectedSection = section;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Output() sectionSelect = new EventEmitter<string>();
   name: string = 'Recipe Management';
+
+  onSelect(section: string) {
+    this.sectionSelect.emit(section);
+  }
 }
