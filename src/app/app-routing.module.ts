4781f5db772665d909b782cba11e31f0bb9b3520
Yuanzheng Li <yuanzhengliwork@gmail.com>
2021-01-14 16:11:55 -0800

    add auth guard for routes that shouldn't be able to visit without logging in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
 
+import { AuthGuardService } from './auth/auth-guard.service';
+import { AuthComponent } from './auth/auth.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListResolver } from './shopping-list/shopping-list-resolver.service';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 const routes: Routes = [
   {
@@ -20,26 +20,31 @@ const routes: Routes = [
     path: 'recipes',
     component: RecipesComponent,
     resolve: [RecipesResolverService],
+    canActivate: [AuthGuardService],
   },
   {
     path: 'recipes/new',
     component: RecipeEditComponent,
     resolve: [RecipesResolverService],
+    canActivate: [AuthGuardService],
   },
   {
     path: 'recipes/:id',
     component: RecipeDetailComponent,
     resolve: [RecipesResolverService],
+    canActivate: [AuthGuardService],
   },
   {
     path: 'recipes/:id/edit',
     component: RecipeEditComponent,
     resolve: [RecipesResolverService],
+    canActivate: [AuthGuardService],
   },
   {
     path: 'shopping-list',
     component: ShoppingListComponent,
     resolve: [ShoppingListResolver],
+    canActivate: [AuthGuardService],
   },
   {
     path: 'auth',
